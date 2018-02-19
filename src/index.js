c8c876376881fb84c9188427719152661f4ffa0a
Alexander Biskop <abiskop@netflix.com>
2018-02-19 08:50:49 -0800

    Tweak performance bar

--- src/index.js
+++ src/index.js
@@ -4,14 +4,9 @@ import '../assets/img/favicon.png';
 const createPalette = () => {
     let i = 0;
     const colors = [
-        '#043D62',
-        'antiquewhite',
-        '#FBF6F6',
-        '#16C8AF',
-        '#7aa9c8',
-        'burlywood',
-        'cadetblue',
-        '#CF7C6D'
+        '#768B99',
+        '#FF7A63',
+        '#CC6766'
     ];
     return {
         next: () => {
@@ -28,11 +23,10 @@ function plotPerformance() {
     const { timing } = window.performance || {};
     if (!timing || !timing.toJSON) { return; }
 
-    const emptyBlock = ({ color }) => {
+    const blockElement = ({ color, percentage }) => {
         const div = document.createElement('div');
         div.setAttribute('style', `
-            flex-grow: 1;
-            transition: flex-grow .5s ease-out;
+            flex-grow: ${percentage};
             background-color: ${color};
         `);
         return div;
@@ -42,33 +36,30 @@ function plotPerformance() {
         .filter(value => value > 0);
     if (timestamps.length < 3) { return; }
 
-    const total = timestamps[timestamps.length - 1] - timestamps[0];
+    const start = timestamps[0];
+    const total = timestamps[timestamps.length - 1] - start;
     const durations = timestamps
+        .slice(1)
         .sort((a, b) => a - b)
         .reduce((series, value) => {
             const previous = series[series.length - 1];
-            const duration = previous ? value - previous.time : 0;
+            const duration = previous ? value - previous.time : value - start;
+            const percentage = Math.floor(100 * duration / total);
             const element = {
                 time: value,
                 duration,
-                percentage: Math.floor(100 * duration / total)
+                percentage,
+                node: blockElement({ color: palette.next(), percentage })
             };
             return series.concat(element);
         }, []);
 
     const container = document.querySelector('[data-perf-container]');
-    const placeholder = document.querySelector('[data-perf-placeholder]');
-    durations.forEach(({ percentage }) => {
-        const div = emptyBlock({ color: palette.next() });
-        container.appendChild(div);
-        setTimeout(() => {
-            requestAnimationFrame(() => {
-                div.style['flex-grow'] = percentage;
-            });
-        }, 0);
+    durations.forEach(({ node }) => {
+        container.appendChild(node);
     });
     requestAnimationFrame(() => {
-        placeholder.style['flex-grow'] = 1;
+        container.style['width'] = '100%';
     });
 }
 
